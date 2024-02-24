1a490e04ce9325bd2b9f253253fe9771faf12aa9
CodeLordGh <babohkossi228@icloud.com>
2024-02-24 17:23:57 +0000

    portfolio section update-restaurant web

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,48 +1,57 @@
-import heroImage from '../assets/heroImage.png'
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
-import { Link } from 'react-scroll'
+import heroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name='home'  className='h-screen pt-20 w-full bg-gradient-to-b from-black
-     via-black to-gray-900'>
-
-      <div className='max-w-screen-lg mx-auto flex flex-col items-center
-        justify-center  px-4 md:flex-row' 
+    <div
+      name="home"
+      className="h-screen pt-20 w-full bg-gradient-to-b from-black
+     via-black to-gray-900"
+    >
+      <div
+        className="max-w-screen-lg mx-auto flex flex-col items-center
+        justify-center  px-4 md:flex-row"
       >
-
-        <div className='flex flex-col my-auto h-full'>
-          <h2 className='text-4xl sm:text-7xl sm:pt-8 font-bold text-white'>
-              I am a Full Stack Developer
+        <div className="flex flex-col my-auto h-full">
+          <h2 className="text-4xl sm:text-7xl sm:pt-8 font-bold text-white">
+            I am a Full Stack Developer
           </h2>
-          <p className='text-gray-500 py-4 max-w-md'>
-            I am a student at ALX SE Program, passionate about learning and building software.
-            I have a strong foundation in HTML, CSS, JavaScript, React, Tailwind CSS, C, Python. 
-            I am also a quick learner and I am always eager to take on new challenges.
+          <p className="text-gray-500 py-4 max-w-md">
+            I am an ALX SE Program graduate, passionate about learning and
+            building software. I have a strong foundation in HTML, CSS,
+            JavaScript, React, Vue, Tailwind CSS, C, Python. I am also a quick
+            learner and I am always eager to take on new challenges.
           </p>
 
           <div>
-            < Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 
               flex items-center rounded-md bg-gradient-to-r from-gray-500 to-blue-500
-              cursor-pointer'
+              cursor-pointer"
             >
               Portfolio
-              <span className='group-hover:rotate-90 duration-300'>
-              
+              <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
           </div>
         </div>
-        <div className='flex flex-col md:shadow-lg rounded-2xl md:shadow-gray-400'>
-            <img src={heroImage} alt='hero profile' className='rounded-2xl
+        <div className="flex flex-col md:shadow-lg rounded-2xl md:shadow-gray-400">
+          <img
+            src={heroImage}
+            alt="hero profile"
+            className="rounded-2xl
             mx-auto w-2/3 h-72 md:w-full md:h-full bg-gradient-to-br from-blue-600
-            to-gray-500'/>
+            to-gray-500"
+          />
         </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,76 +1,101 @@
-import todo from '../assets/portfolio/todo.png'
-import installNode from '../assets/portfolio/installNode.jpg'
-import navbar from '../assets/portfolio/navbar.jpg'
-import reactParallax from '../assets/portfolio/reactParallax.jpg'
-import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
-import reactWeather from '../assets/portfolio/reactWeather.jpg'
-
+import todo from "../assets/portfolio/todo.png";
+import reactwebsite from "../assets/portfolio/restaurantWeb.jpg";
+// import navbar from "../assets/portfolio/navbar.jpg";
+// import reactParallax from "../assets/portfolio/reactParallax.jpg";
+// import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+// import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 const Portfolio = () => {
-
-    const portfolios = [
-        {
-            id: 1,
-            src: todo,
-            demohref: "https://react-todo-list-two-xi.vercel.app/",
-            codefref: "https://github.com/CodeLordGh/reactTodoList",
-        },
-        // {
-        //     id: 2,
-        //     src: installNode,
-        // },
-        // {
-        //     id: 3,
-        //     src: navbar,
-        // },
-        // {
-        //     id: 4,
-        //     src: reactSmooth,
-        // },
-        // {
-        //     id: 5,
-        //     src: reactParallax,
-        // },
-        // {
-        //     id: 6,
-        //     src: reactWeather,
-        // }
-    ]
+  const portfolios = [
+    {
+      id: 1,
+      src: todo,
+      demohref: "https://react-todo-list-two-xi.vercel.app/",
+      codefref: "https://github.com/CodeLordGh/reactTodoList",
+    },
+    {
+      id: 2,
+      src: reactwebsite,
+      demohref: "",
+      codefref: "https://github.com/CodeLordGh/restaurantWeb",
+    },
+    // },
+    // {
+    //     id: 3,
+    //     src: navbar,
+    // },
+    // {
+    //     id: 4,
+    //     src: reactSmooth,
+    // },
+    // {
+    //     id: 5,
+    //     src: reactParallax,
+    // },
+    // {
+    //     id: 6,
+    //     src: reactWeather,
+    // }
+  ];
 
   return (
-    <div name='portfolio' className='bg-gradient-to-b from-black
-    to-gray-500 w-full text-white md:h-screen'>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col 
-      justify-center w-full h-full'>
-        <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4
-            border-gray-500'>Portfolio</p>
-            <p className='py-6'>Checkout some of my work right here</p>
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black
+    to-gray-500 w-full text-white md:h-screen"
+    >
+      <div
+        className="max-w-screen-lg p-4 mx-auto flex flex-col 
+      justify-center w-full h-full"
+      >
+        <div className="pb-8">
+          <p
+            className="text-4xl font-bold inline border-b-4
+            border-gray-500"
+          >
+            Portfolio
+          </p>
+          <p className="py-6">Checkout some of my work right here</p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3
-        gap-8 px-12 sm:px-0'>
-
-            {
-                portfolios.map(({id, src, codefref, demohref}) => {
-                    return (
-                            <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
-                                <img src={src} alt="reactWeather" className='rounded-md
-                                duration-300 hover:scale-105' />
-                                <div className='flex items-center justify-center'>
-                                    <button className='w12 px-6 py-3 m-4 duration-200 
-                                    hover:scale-150'><a target='_blank' rel='noreferrer' href={demohref}>Demo</a></button>
-                                    <button className='w12 px-6 py-3 m-4 duration-200 
-                                    hover:scale-150'><a href={codefref} rel='noreferrer' target='_blank'>Code</a></button>
-                                </div>
-                            </div>
-                    )
-                })
-            }
+        <div
+          className="grid sm:grid-cols-2 md:grid-cols-3
+        gap-8 px-12 sm:px-0"
+        >
+          {portfolios.map(({ id, src, codefref, demohref }) => {
+            return (
+              <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+                <img
+                  src={src}
+                  alt="reactWeather"
+                  className="rounded-md
+                                duration-300 hover:scale-105"
+                />
+                <div className="flex items-center justify-center">
+                  <button
+                    className="w12 px-6 py-3 m-4 duration-200 
+                                    hover:scale-150"
+                  >
+                    <a target="_blank" rel="noreferrer" href={demohref}>
+                      Demo
+                    </a>
+                  </button>
+                  <button
+                    className="w12 px-6 py-3 m-4 duration-200 
+                                    hover:scale-150"
+                  >
+                    <a href={codefref} rel="noreferrer" target="_blank">
+                      Code
+                    </a>
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
